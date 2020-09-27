eb476fd2093b5764ac251bae81eb4426c4d02505
fznnshr <fznnshr@gmail.com>
2020-09-27 16:44:43 +0700

    Menambahkan data dan grafik

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ for (let i = 0; i < navlink.length; i++) {
     console.log("nav-link");
   });
 }
+
+TESTER = document.getElementById("tester");
+
+// z_data[y][x] = z
+var z_data = [
+  [0, 0, 0, 0],
+  [0, 50, 55, 50],
+  [0, 45, 45, 45],
+  [0, 40, 40, 40],
+];
+
+var data = [
+  {
+    z: z_data,
+    type: "surface",
+  },
+];
+
+var layout = {
+  //   title: "Tekanan Bunyi Pada Ruangan",
+  autosize: true,
+  //   width: 600,
+  //   height: 500,
+  scene: {
+    xaxis: { title: "Room Depth" },
+    yaxis: { title: "Room Width" },
+    zaxis: { title: "Sound Intensity" },
+  },
+  margin: {
+    l: 0,
+    r: 0,
+    b: 0,
+    t: 0,
+  },
+};
+Plotly.newPlot(TESTER, data, layout);
